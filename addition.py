c6fc3e73b6ed5c99027a97db6a4e5b875bd77998
jayasuriya <56404117+Jayasuriya007@users.noreply.github.com>
2020-12-12 21:21:35 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,11 @@
+print ("Program To Find Sum of Three Digits")
+def addition  (a,b,c):
+    result=(a+b+c)
+    return result
+
+a= int(input("Enter num one: "))
+b= int(input("Enter num one: "))
+c= int(input("Enter num one: "))
+add_result= addition(a,b,c)
+print(add_result)
+
